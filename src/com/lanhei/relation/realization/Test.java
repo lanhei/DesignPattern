8ca1feae81d811d5b0c537e13ff4e15260c8beb2
lanhei <glay126@126.com>
2019-07-10 17:24:35 +0800

    提交

---
+++ src/com/lanhei/relation/realization/Test.java
@@ -0,0 +1,10 @@
+package com.lanhei.relation.realization;
+
+public class Test {
+    public static void main(String[] args) {
+        Pepole stuent=new Student();
+        Pepole teacher=new Teacher();
+        stuent.sleep();
+        teacher.sleep();
+    }
+}
