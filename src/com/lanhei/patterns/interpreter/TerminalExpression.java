cf0d5c15b9cb300c5d94e09267b9218c25816ce8
lanhei <glay126@126.com>
2019-08-13 09:53:21 +0800

    解释器模式

---
+++ src/com/lanhei/patterns/interpreter/TerminalExpression.java
@@ -0,0 +1,17 @@
+package com.lanhei.patterns.interpreter;
+
+public class TerminalExpression implements Expression {
+    private String data;
+
+    public TerminalExpression(String data){
+        this.data = data;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        if(context.contains(data)){
+            return true;
+        }
+        return false;
+    }
+}
