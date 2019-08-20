2e816f4bca7ba359ad4ab4070362646be19ff920
lanhei <glay126@126.com>
2019-08-20 10:19:09 +0800

    备忘录模式

---
+++ src/com/lanhei/patterns/memento/Memento.java
@@ -0,0 +1,13 @@
+package com.lanhei.patterns.memento;
+
+public class Memento {
+    private String state;
+
+    public Memento(String state){
+        this.state = state;
+    }
+
+    public String getState(){
+        return state;
+    }
+}
